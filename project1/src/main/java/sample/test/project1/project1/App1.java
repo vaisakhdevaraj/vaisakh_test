62f5fc135ede5822671b8529f1f8b3dc3c09a8b6
921089 <921089@12HW000589>
2017-04-28 11:32:05 +0300

    commited

---
+++ project1/src/main/java/sample/test/project1/project1/App1.java
@@ -0,0 +1,13 @@
+package sample.test.project1.project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
